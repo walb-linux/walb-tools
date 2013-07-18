2e478021650fcb454bc7e1d3ce20bc6bcc37a405
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2013-07-18 17:17:53 +0900

    implement wdev-poll tool. This is still a toy.

---
+++ src/walb_log_poll.hpp
@@ -0,0 +1,239 @@
+/**
+ * @file
+ * @brief walb log device polling.
+ * @author HOSHINO Takashi
+ *
+ * (C) 2013 Cybozu Labs, Inc.
+ */
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <mutex>
+#include <vector>
+#include <string>
+#include <map>
+
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#ifndef WALB_LOG_POLL_HPP
+#define WALB_LOG_POLL_HPP
+
+namespace walb {
+namespace log {
+
+/**
+ * Get polling path.
+ *
+ * @wdevName walb device name.
+ * RETURN:
+ *   full path of polling target.
+ */
+std::string getPollingPath(const std::string &wdevName)
+{
+    return cybozu::util::formatString(
+        "/sys/block/walb!%s/walb/lsids", wdevName.c_str());
+}
+
+/**
+ * This is thread safe.
+ *
+ * Typical usage:
+ *   Thread 0: just call poll() and
+ *     invoke another tasks with returned wdev name list.
+ *   Thread i: call add()/del() when the control command received.
+ */
+class WalbLogPoller
+{
+private:
+    mutable std::mutex mutex_;
+    int efd_;
+    std::vector<struct epoll_event> ev_;
+    std::map<std::string, int> fdMap_; /* name, fd. */
+    std::map<int, std::string> nameMap_; /* fd, name. */
+
+public:
+    explicit WalbLogPoller(unsigned int maxEvents = 1)
+        : mutex_()
+        , efd_(-1)
+        , ev_(maxEvents)
+        , fdMap_()
+        , nameMap_() {
+        assert(0 < maxEvents);
+        efd_ = ::epoll_create(maxEvents);
+        if (efd_ < 0) {
+            throw std::runtime_error("epoll_create failed.");
+        }
+    }
+    ~WalbLogPoller() noexcept {
+        auto it = fdMap_.begin();
+        while (it != fdMap_.end()) {
+            int fd = it->second;
+            if (::epoll_ctl(efd_, EPOLL_CTL_DEL, fd, nullptr) < 0) {
+                /* failed but do nothing. */
+            }
+            ::close(fd);
+            it = fdMap_.erase(it);
+        }
+        nameMap_.clear();
+        ::close(efd_);
+        efd_ = -1;
+    }
+    /**
+     * DO NOT call this from multiple threads.
+     * This will throw an exception if caught SIGINT.
+     *
+     * TODO: signal handling.
+     *   Should we use epoll_pwait()?
+     */
+    std::vector<std::string> poll(int timeoutMs = -1) {
+        std::vector<std::string> v;
+        int nfds = ::epoll_wait(efd_, &ev_[0], ev_.size(), timeoutMs);
+        if (nfds < 0) {
+            /* TODO: logging. */
+            return std::move(v);
+        }
+        std::lock_guard<std::mutex> lk(mutex_);
+        for (int i = 0; i < nfds; i++) {
+            int fd = ev_[i].data.fd;
+            std::string name = getName(fd);
+            if (!name.empty()) {
+                v.push_back(name);
+                char buf[4096];
+                ::lseek(fd, 0, SEEK_SET);
+                UNUSED int s = ::read(fd, buf, 4096);
+#ifdef DEBUG
+                ::printf("read %d bytes\n", s);
+#endif
+            }
+        }
+        return std::move(v);
+    }
+    /**
+     * If already exists, call del() and add().
+     */
+    bool addForce(const std::string &wdevName) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        delNolock(wdevName);
+        return addNolock(wdevName);
+    }
+    /**
+     * Add a walb device to the polling targets.
+     * @wdevName walb device name. This is unique in a host.
+     */
+    bool add(const std::string &wdevName) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return addNolock(wdevName);
+    }
+    /**
+     * Del a walb device from the polling targets.
+     */
+    void del(const std::string &wdevName) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        delNolock(wdevName);
+    }
+    /**
+     * Get list of wdevName and polling fd.
+     */
+    std::vector<std::pair<std::string, int> > list() const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        std::vector<std::pair<std::string, int> > v;
+        for (const auto &p : fdMap_) {
+            v.push_back(p);
+        }
+        return std::move(v);
+    }
+private:
+    bool addName(const std::string &wdevName, int fd) {
+        auto p0 = fdMap_.insert(std::make_pair(wdevName, fd));
+        if (!p0.second) {
+            return false;
+        }
+        auto p1 = nameMap_.insert(std::make_pair(fd, wdevName));
+        if (!p1.second) {
+            fdMap_.erase(p0.first);
+            return false;
+        }
+        assert(fdMap_.size() == nameMap_.size());
+        return true;
+    }
+    void delName(const std::string &wdevName, int fd) {
+        auto it0 = fdMap_.find(wdevName);
+        if (it0 != fdMap_.end()) {
+            assert(it0->second == fd);
+            fdMap_.erase(it0);
+        }
+        auto it1 = nameMap_.find(fd);
+        if (it1 != nameMap_.end()) {
+            assert(it1->second == wdevName);
+            nameMap_.erase(it1);
+        }
+        assert(fdMap_.size() == nameMap_.size());
+    }
+    /**
+     * RETURN:
+     *   -1 if not found.
+     */
+    int getFd(const std::string &wdevName) const {
+        auto it = fdMap_.find(wdevName);
+        if (it == fdMap_.end()) return -1;
+        return it->second;
+    }
+    /**
+     * RETURN:
+     *   "" if not found.
+     */
+    std::string getName(int fd) const {
+        auto it = nameMap_.find(fd);
+        if (it == nameMap_.end()) return "";
+        return it->second;
+    }
+    /**
+     * Add an walb device.
+     * RETURN:
+     *   false when some error occurs.
+     */
+    bool addNolock(const std::string &wdevName) {
+        std::string path = getPollingPath(wdevName);
+        int fd = ::open(path.c_str(), O_RDONLY);
+        if (fd < 0) goto error0;
+
+        if (!addName(wdevName, fd)) goto error1;
+
+        struct epoll_event ev;
+        ::memset(&ev, 0, sizeof(ev));
+        ev.events = EPOLLIN | EPOLLET;
+        ev.data.fd = fd;
+        if (::epoll_ctl(efd_, EPOLL_CTL_ADD, fd, &ev) < 0) goto error2;
+        return true;
+
+      error2:
+        delName(wdevName, fd);
+      error1:
+        ::close(fd);
+      error0:
+        return false;
+    }
+    /**
+     * Delete an walb device.
+     * If not found, do nothing.
+     */
+    void delNolock(const std::string &wdevName) {
+        int fd = getFd(wdevName);
+        if (fd < 0) return;
+        delName(wdevName, fd);
+        if (::epoll_ctl(efd_, EPOLL_CTL_DEL, fd, nullptr) < 0) {
+            throw std::runtime_error("epoll_ctl failed.");
+        }
+        if (::close(fd) != 0) {
+            throw std::runtime_error("close failed.");
+        }
+    }
+};
+
+}} //namespace walb::log
+
+#endif /* WALB_LOG_POLL_HPP */
---
+++ src/wdev-poll.cpp
@@ -0,0 +1,143 @@
+/**
+ * @file
+ * @brief polling walb log generation.
+ * @author HOSHINO Takashi
+ *
+ * (C) 2013 Cybozu Labs, Inc.
+ */
+#include <thread>
+#include <atomic>
+
+#include "cybozu/option.hpp"
+#include "util.hpp"
+#include "walb_log_poll.hpp"
+
+struct Option : public cybozu::Option
+{
+    std::vector<std::string> wdevNameV;
+    Option() {
+        setUsage("Usage: wdev-poll [wdev name list]");
+        appendVec(&wdevNameV, "i", "wdev name list");
+        appendHelp("h");
+    }
+    bool parse(int argc, char *argv[]) {
+        if (!cybozu::Option::parse(argc, argv)) return false;
+        //if (wdevNameV.empty()) goto error;
+        return true;
+      error:
+        usage();
+        return false;
+    }
+};
+
+void pollWorker(std::atomic<bool> &flag, walb::log::WalbLogPoller &poller)
+{
+    try {
+        while (!flag.load()) {
+            auto v = poller.poll(1000);
+            ::printf("got %zu\n", v.size());
+            for (std::string &s : v) {
+                ::printf("wdev %s\n", s.c_str());
+            }
+        }
+    } catch (...) {
+    }
+}
+
+size_t nonSpacePos(const std::string &s, size_t pos)
+{
+    size_t i = pos;
+    while (i < s.size()) {
+        if (s[i] != ' ') break;
+        ++i;
+    }
+    return i;
+}
+
+std::vector<std::string> commandReader(FILE *fp)
+{
+    char buf[1024];
+    if (::fgets(buf, 1024, fp) == nullptr) {
+        ::printf("fgets failed.\n");
+        return {"quit"};
+    }
+    int l = ::strlen(buf);
+    if (0 < l && buf[l - 1] == '\n') {
+        buf[l - 1] = '\0';
+    }
+    std::string s(buf);
+    std::vector<std::string> v;
+
+    size_t n = s.find(" ");
+    while (n != std::string::npos) {
+        v.push_back(s.substr(0, n));
+        s = s.substr(nonSpacePos(s, n));
+        n = s.find(" ");
+    }
+    if (!s.empty()) {
+        v.push_back(s);
+    }
+    return std::move(v);
+}
+
+bool commandRunner(const std::vector<std::string> &cmds, walb::log::WalbLogPoller &poller)
+{
+    if (cmds.empty() || cmds[0] == "quit") return false;
+
+    const std::string &cmdType = cmds[0];
+    std::string wdevName;
+    if (1 <= cmds.size()) {
+        wdevName = cmds[1];
+    }
+    if (cmdType == "addForce") {
+        ::printf("addForce '%s'(%zu)\n", wdevName.c_str(), wdevName.size());
+        if (!poller.addForce(wdevName)) {
+            ::printf("addForce failed.\n");
+        }
+    } else if (cmdType == "add") {
+        ::printf("add '%s'(%zu)\n", wdevName.c_str(), wdevName.size());
+        if (!poller.add(wdevName)) {
+            ::printf("add failed.\n");
+        }
+    } else if (cmdType == "del") {
+        ::printf("del '%s'(%zu)\n", wdevName.c_str(), wdevName.size());
+        poller.del(wdevName);
+    } else if (cmdType == "list") {
+        for (auto &p : poller.list()) {
+            ::printf("%s %d\n", p.first.c_str(), p.second);
+        }
+    } else {
+        ::printf("bad command: '%s' %zu\n", cmdType.c_str(), cmdType.size());
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    try {
+        walb::log::WalbLogPoller poller;
+
+        Option opt;
+        if (!opt.parse(argc, argv)) {
+            return 1;
+        }
+        std::atomic<bool> flag(false);
+
+        std::thread th(pollWorker, std::ref(flag), std::ref(poller));
+        while (true) {
+            std::vector<std::string> cmds = commandReader(::stdin);
+            if (!commandRunner(cmds, poller)) {
+                break;
+            }
+        }
+        flag.store(true);
+        th.join();
+
+        return 0;
+    } catch (...) {
+        ::printf("caught an error.");
+        return 1;
+    }
+}
+/* end of file. */
