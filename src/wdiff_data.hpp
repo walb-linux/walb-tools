d50d9398174a0b1c962be08901bef48ad6ef4ad7
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2013-07-29 17:15:58 +0900

    wdiff_data: r20130729a

--- src/wdiff_data.hpp
+++ src/wdiff_data.hpp
@@ -8,6 +8,8 @@
 #include <cassert>
 #include <map>
 #include <mutex>
+#include <vector>
+#include <queue>
 #include <time.h>
 #include "cybozu/serializer.hpp"
 #include "cybozu/file.hpp"
@@ -39,6 +41,11 @@ namespace walb {
  *   ts0-c-gid0-gid1.wdiff + ts1-0-gid1-gid2.wdiff --> ts1-c-gid0-gid2.wdiff
  *   ts0 <= ts1.
  *   See MetaDiff::canMerge() for detail.
+ *
+ * LatestRecord file:
+ *   This indicates the latest snapshot gid(s).
+ *   The file contents is a serialized MetaSnap object.
+ *   gid0, gid1, and timestamp are meaningfull.
  */
 class WalbDiffFiles
 {
@@ -50,12 +57,22 @@ private:
     mutable std::mutex mutex_; /* for mmap_. */
 
 public:
-    WalbDiffFiles(const std::string &dirStr)
+    WalbDiffFiles(const std::string &dirStr, bool doesReset = false)
         : dir_(dirStr), mmap_(), mutex_() {
         if (!dir_.stat().isDirectory()) {
             throw std::runtime_error("Directory does not exist.");
         }
-        reloadMetadata();
+        if (doesReset) {
+            if (!dir_.rmdirRecursive()) {
+                throw std::runtime_error("Reset (rmdirRecursive) failed.");
+            }
+            if (!dir_.mkdir()) {
+                throw std::runtime_error("Reset (mkdir) failed.");
+            }
+            reset(0);
+        } else {
+            reloadMetadata();
+        }
     }
     ~WalbDiffFiles() noexcept {
         try {
@@ -64,44 +81,162 @@ public:
         }
     }
     /**
-     * Merge gid range [gid0, gid1) into a wdiff file.
-     *
-     * Old wdiff files will not be removed.
-     * You must call cleanup() explicitly.
+     * CAUSION:
+     *   All data inside the directory will be removed.
      */
-    void merge(uint64_t gid0, uint64_t gid1) {
-        /* now editing */
+    void reset(uint64_t gid) {
+        /* remove all wdiff files. */
+        removeBeforeGid(uint64_t(-1));
+
+        latestRecord_.init();
+        latestRecord_.raw().gid0 = gid;
+        latestRecord_.raw().gid1 = gid;
+        saveLatestRecord();
     }
     /**
      * Add a diff and update the "latest" record file.
+     * Before calling this, you must settle the diff file.
      */
-    void add(const MetaDiff &diff) {
-        /* now editing */
+    bool add(const MetaDiff &diff) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        if (!latestRecord_.canApply(diff)) return false;
+        mmap_.insert(std::make_pair(diff.gid0(), diff));
+        latestRecord_ = latestRecord_.apply(diff);
+        latestRecord_.raw().timestamp = diff.raw().timestamp;
+        saveLatestRecord();
+        return true;
+    }
+    /**
+     * Merge gid range [gid0, gid1) into a wdiff.
+     *
+     * You must call this after created the merged diff file.
+     * Old wdiff files will not be removed.
+     * You must call cleanup() explicitly.
+     *
+     * There must be diffs which gid0/gid1 is the given gid0/gid1.
+     * and there is no diff which can_merge are false between them.
+     *
+     * RETURN:
+     *   true if successfuly merged.
+     *   or false.
+     */
+    bool merge(uint64_t gid0, uint64_t gid1) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        Mmap::const_iterator it = mmap_.lower_bound(gid0);
+        if (it == mmap_.cend()) return false;
+        MetaDiff merged, diff;
+        it = nextKey(it, diff);
+        merged = diff;
+        if (diff.gid0() != gid0) return false;
+        it = skipOverlapped(it, diff.gid1());
+        while (it != mmap_.cend()) {
+            it = nextKey(it, diff);
+            if (gid1 < diff.gid1()) break;
+            if (!merged.canMerge(diff)) return false;
+            merged = merged.merge(diff);
+            merged.raw().timestamp = diff.raw().timestamp;
+            it = skipOverlapped(it, diff.gid1());
+        }
+        assert(merged.gid0() == gid0);
+        assert(merged.gid1() == gid1);
+        mmap_.insert(std::make_pair(merged.gid0(), merged));
+        return true;
+    }
+    /**
+     * Search diffs which can be merged.
+     * RETURN:
+     *   A list of gid ranges.
+     */
+    std::vector<std::pair<uint64_t, uint64_t> > getMergeCandidates() const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        std::queue<MetaDiff> q;
+        std::vector<std::pair<uint64_t, uint64_t> > v;
+        auto insert = [&]() {
+            uint64_t bgn = 0, end = 0;
+            assert(!q.empty());
+            if (q.size() == 1) {
+                q.pop();
+                return;
+            }
+            bgn = q.front().gid0();
+            while (!q.empty()) {
+                end = q.front().gid1();
+                q.pop();
+            }
+            v.push_back(std::make_pair(bgn, end));
+        };
+        for (const MetaDiff &diff : listDiffNolock()) {
+            if (q.empty() || diff.raw().can_merge) {
+                q.push(diff);
+            } else {
+                insert();
+                q.push(diff);
+            }
+        }
+        insert();
+        return std::move(v);
     }
     /**
      * Remove wdiffs before a specified gid.
      */
     void removeBeforeGid(uint64_t gid) {
-        /* now editing */
+        std::lock_guard<std::mutex> lk(mutex_);
+        removeBeforeGidNolock(gid);
     }
     /**
      * Remove wdiffs before a specified timestamp.
      */
-    void removeBeforeTime(uint64_t ts) {
-        /* now editing */
+    void removeBeforeTime(uint64_t timestamp) {
+        std::lock_guard<std::mutex> lk(mutex_);
+        uint64_t gid1 = getMaxGidBeforeTime(timestamp);
+        removeBeforeGidNolock(gid1);
+    }
+    /**
+     * List wdiff name before a given timestamp.
+     */
+    std::vector<std::string> listBeforeTime(uint64_t timestamp) const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return convertDiffToName(listDiffNolock(0, getMaxGidBeforeTime(timestamp)));
     }
     /**
      * List of wdiff name in order of gid.
+     * RETURN:
+     *   list of wdiff name.
+     *   the list does not contain overlapped ones.
      */
-    std::vector<std::string> list() const {
-        /* now editing */
-        return {};
+    std::vector<std::string> listName() const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return convertDiffToName(listDiffNolock());
+    }
+    std::vector<std::string> listName(uint64_t gid0, uint64_t gid1) const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return convertDiffToName(listDiffNolock(gid0, gid1));
+    }
+    /**
+     * RETURN:
+     *   list of diff.
+     *   the list does not contain overlapped ones.
+     */
+    std::vector<MetaDiff> listDiff() const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return listDiffNolock();
+    }
+    std::vector<MetaDiff> listDiff(uint64_t gid0, uint64_t gid1) const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return listDiffNolock(gid0, gid1);
     }
     /**
      * Remove wdiff files which have been already merged.
      */
     void cleanup() {
-        /* now editing */
+        std::lock_guard<std::mutex> lk(mutex_);
+        Mmap::iterator it;
+        it = mmap_.begin();
+        while (it != mmap_.end()) {
+            MetaDiff diff;
+            nextKey(it, diff);
+            it = removeOverlapped(it, diff);
+        }
     }
     /**
      * Get the latest gid in the directory.
@@ -125,31 +260,23 @@ public:
         }
     }
     /**
+     * Get the latest snapshot record.
+     */
+    MetaSnap latestSnap() const {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return latestRecord_;
+    }
+    /**
      * Reload metadata by scanning directory entries.
      * searching "*.wdiff" files.
      */
     void reloadMetadata() {
         std::lock_guard<std::mutex> lk(mutex_);
-        mmap_.clear();
-        std::vector<cybozu::FileInfo> list;
-        if (!cybozu::GetFileList(list, dir_.str(), "wdiff")) {
-            throw std::runtime_error("GetFileList failed.");
-        }
-        for (cybozu::FileInfo &info : list) {
-            MetaDiff diff;
-            if (!parseDiffFileName(info.name, diff)) continue;
-            mmap_.insert(std::make_pair(diff.gid0(), diff));
-        }
-        checkNolock();
-        if (mmap_.empty()) {
-            loadLatestRecord();
-        } else {
-            MetaDiff diff;
-            prevKey(mmap_.rbegin(), diff);
-            latestRecord_ = getSnapFromDiff(diff);
-        }
+        reloadMetadataNolock();
     }
-private:
+    const cybozu::FilePath &dirPath() const {
+        return dir_;
+    }
     /**
      * Create a diff file name.
      */
@@ -232,6 +359,7 @@ private:
         }
         return diff.isValid();
     }
+private:
     /**
      * Check whether whole data of oldestGid() <= gid < latestGid() exist.
      */
@@ -324,6 +452,138 @@ private:
         cybozu::TmpFile tmpFile(dir_.str());
         cybozu::save(tmpFile, latestRecord_);
         tmpFile.save(latestRecordPath().str());
+    }
+    /**
+     * See reloadMetadata();
+     */
+    void reloadMetadataNolock() {
+        mmap_.clear();
+        std::vector<cybozu::FileInfo> list;
+        if (!cybozu::GetFileList(list, dir_.str(), "wdiff")) {
+            throw std::runtime_error("GetFileList failed.");
+        }
+        for (cybozu::FileInfo &info : list) {
+            MetaDiff diff;
+            if (!parseDiffFileName(info.name, diff)) continue;
+            mmap_.insert(std::make_pair(diff.gid0(), diff));
+        }
+        checkNolock();
+        if (mmap_.empty()) {
+            loadLatestRecord();
+        } else {
+            MetaDiff diff;
+            prevKey(mmap_.rbegin(), diff);
+            latestRecord_ = getSnapFromDiff(diff);
+            saveLatestRecord();
+        }
+    }
+    /**
+     * Remove diff files.
+     */
+    void removeFiles(const std::vector<std::string> &pathStrV) const {
+        for (const std::string &pathStr : pathStrV) {
+            cybozu::FilePath p = dir_ + cybozu::FilePath(pathStr);
+            if (!p.stat().isFile()) continue;
+            if (!p.unlink()) {
+                /* TODO: put log. */
+            }
+        }
+    }
+    /**
+     * See removeBeforeGid().
+     */
+    void removeBeforeGidNolock(uint64_t gid1) {
+        std::vector<std::string> v;
+        Mmap::iterator it = mmap_.begin();
+        while (it != mmap_.end()) {
+            MetaDiff &diff = it->second;
+            if (diff.gid1() <= gid1) {
+                it = mmap_.erase(it);
+                v.push_back(createDiffFileName(diff));
+            } else {
+                ++it;
+            }
+        }
+        removeFiles(v);
+    }
+    /**
+     * CAUSION:
+     *   the lock must be held.
+     * RETURN:
+     *   maximum gid1 among wdiffs which timestmap are all before a given timestamp.
+     */
+    uint64_t getMaxGidBeforeTime(uint64_t timestamp) const {
+        uint64_t gid1 = 0;
+        Mmap::const_iterator it = mmap_.begin();
+        while (it != mmap_.cend()) {
+            const MetaDiff &diff = it->second;
+            if (diff.raw().timestamp < timestamp && gid1 < diff.gid1()) {
+                gid1 = diff.gid1();
+            }
+            ++it;
+        }
+        return gid1;
+    }
+    /**
+     * Remove overlapped diff files.
+     *
+     * @it iterator that indicates the first element of the target range.
+     * @diff the function will remove diffs that are overwritten by it.
+     *     the diff itself will not be removed.
+     *
+     * RETURN:
+     *   An iterator follows of the next key.
+     *   Its gid0 must be the gid1 of the given diff if valid.
+     */
+    Mmap::iterator removeOverlapped(Mmap::iterator it, const MetaDiff &diff) {
+        std::vector<std::string> v;
+        while (it != mmap_.end() && it->second.gid1() <= diff.gid1()) {
+            MetaDiff &diff0 = it->second;
+            assert(diff.gid0() <= diff0.gid0());
+            if (diff0 == diff) {
+                ++it;
+                continue;
+            }
+            v.push_back(createDiffFileName(diff0));
+            it = mmap_.erase(it);
+        }
+#ifdef DEBUG
+        if (it != mmap_.end()) {
+            MetaDiff &diff0 = it->second;
+            assert(diff.gid1() == diff0.gid0());
+        }
+#endif
+        removeFiles(v);
+        return it;
+    }
+    /**
+     * See listDiff().
+     */
+    std::vector<MetaDiff> listDiffNolock(uint64_t gid0, uint64_t gid1) const {
+        Mmap::const_iterator it = mmap_.lower_bound(gid0);
+        if (it == mmap_.cend()) return {};
+        std::vector<MetaDiff> v;
+        MetaDiff diff;
+        while (it != mmap_.cend()) {
+            it = nextKey(it, diff);
+            if (gid1 < diff.gid1()) break;
+            v.push_back(diff);
+            it = skipOverlapped(it, diff.gid1());
+        }
+        return std::move(v);
+    }
+    std::vector<MetaDiff> listDiffNolock() const {
+        return listDiffNolock(0, uint64_t(-1));
+    }
+    /**
+     * Convert diff list to name list.
+     */
+    static std::vector<std::string> convertDiffToName(const std::vector<MetaDiff> &diffV) {
+        std::vector<std::string> v;
+        for (const MetaDiff &diff : diffV) {
+            v.push_back(createDiffFileName(diff));
+        }
+        return std::move(v);
     }
 };
 
---
+++ utest/test_wdiff_data.cpp
@@ -0,0 +1,93 @@
+#include "cybozu/test.hpp"
+#include "wdiff_data.hpp"
+#include "file_path.hpp"
+
+void setDiff(walb::MetaDiff &diff, uint64_t gid0, uint64_t gid1, bool canMerge)
+{
+    diff.init();
+    diff.raw().gid0 = gid0;
+    diff.raw().gid1 = gid1;
+    diff.raw().gid2 = gid1;
+    diff.raw().can_merge = canMerge;
+};
+
+void createFile(walb::WalbDiffFiles &diffFiles, walb::MetaDiff &diff)
+{
+    cybozu::FilePath fp = diffFiles.dirPath() + cybozu::FilePath(diffFiles.createDiffFileName(diff));
+    cybozu::util::createEmptyFile(fp.str());
+}
+
+class TestDirectory
+{
+private:
+    cybozu::FilePath fp_;
+public:
+    explicit TestDirectory(const std::string &path)
+        : fp_(path) {
+        if (fp_.stat().exists()) {
+            throw std::runtime_error("directory already exists.");
+        }
+        if (!fp_.mkdir()) {
+            throw std::runtime_error("mkdir() failed.");
+        }
+    }
+    ~TestDirectory() noexcept {
+        try {
+            fp_.rmdirRecursive();
+        } catch (...) {
+        }
+    }
+};
+
+CYBOZU_TEST_AUTO(merge)
+{
+    cybozu::FilePath fp("test_wdiff_files_dir0");
+    TestDirectory testDir(fp.str());
+
+    walb::WalbDiffFiles diffFiles(fp.str(), true);
+    walb::MetaDiff diff;
+    setDiff(diff, 0, 1, false); diffFiles.add(diff); createFile(diffFiles, diff);
+    setDiff(diff, 1, 2, true);  diffFiles.add(diff); createFile(diffFiles, diff);
+    setDiff(diff, 2, 3, false); diffFiles.add(diff); createFile(diffFiles, diff);
+    setDiff(diff, 3, 4, true);  diffFiles.add(diff); createFile(diffFiles, diff);
+    setDiff(diff, 4, 5, true);  diffFiles.add(diff); createFile(diffFiles, diff);
+    for (const std::string &s : diffFiles.listName()) {
+        ::printf("%s\n", s.c_str());
+    }
+    CYBOZU_TEST_EQUAL(diffFiles.listName().size(), 5);
+
+    std::vector<std::pair<uint64_t, uint64_t> > v = diffFiles.getMergeCandidates();
+    CYBOZU_TEST_EQUAL(v.size(), 2);
+    ::printf("%" PRIu64 " %" PRIu64 "\n", v[0].first, v[0].second);
+
+    CYBOZU_TEST_EQUAL(v[0].first, 0);
+    CYBOZU_TEST_EQUAL(v[0].second, 2);
+    CYBOZU_TEST_EQUAL(v[1].first, 2);
+    CYBOZU_TEST_EQUAL(v[1].second, 5);
+
+    setDiff(diff, 0, 2, false);
+    createFile(diffFiles, diff);
+    CYBOZU_TEST_ASSERT(diffFiles.merge(0, 2));
+
+    setDiff(diff, 2, 5, false);
+    createFile(diffFiles, diff);
+    CYBOZU_TEST_ASSERT(diffFiles.merge(2, 5));
+
+    for (const std::string &s : diffFiles.listName()) {
+        ::printf("%s\n", s.c_str());
+    }
+    CYBOZU_TEST_EQUAL(diffFiles.listName().size(), 2);
+    diffFiles.removeBeforeGid(5);
+
+    setDiff(diff, 5, 127, false); diffFiles.add(diff); createFile(diffFiles, diff);
+    diffFiles.reloadMetadata();
+    CYBOZU_TEST_EQUAL(diffFiles.latestGid(), 127);
+    std::vector<walb::MetaDiff> diffV = diffFiles.listDiff();
+    CYBOZU_TEST_EQUAL(diffV.size(), 1);
+    CYBOZU_TEST_EQUAL(diffV[0].gid0(), 5);
+    CYBOZU_TEST_EQUAL(diffV[0].gid1(), 127);
+
+    diffFiles.reset(10000);
+    setDiff(diff, 10000, 10001, false); diffFiles.add(diff); createFile(diffFiles, diff);
+    CYBOZU_TEST_EQUAL(diffFiles.latestGid(), 10001);
+}
