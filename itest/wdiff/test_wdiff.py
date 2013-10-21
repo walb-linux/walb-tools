bc7118d7e90050ca9138fb5f289ca0d248d78031
MITSUNARI Shigeo <herumi@nifty.com>
2013-10-21 15:12:01 +0900

    new itest by Python

--- itest/wdiff/test_wdiff.py
+++ itest/wdiff/test_wdiff.py
@@ -1,32 +1,8 @@
-import subprocess, shlex, sys
+import sys
+sys.path.append("../")
+from run import *
 
 BIN="../../binsrc"
-
-def run(args):
-    print "args:", args
-    cmd = shlex.split(args)
-    inFile = None
-    outFile = None
-    if "<" in cmd:
-        pos = cmd.index("<")
-        inFile = open(cmd[pos + 1], "r")
-        cmd[pos:pos+2] = []
-    if ">" in cmd:
-        pos = cmd.index(">")
-        outFile = open(cmd[pos + 1], "w")
-        cmd[pos:pos+2] = []
-
-    p = subprocess.Popen(cmd, stdin=inFile, stdout=outFile, stderr=sys.stdout)
-    ret = p.wait()
-    if inFile:
-        inFile.close()
-    if outFile:
-        outFile.close()
-    if ret:
-        raise EnvironmentError(cmd)
-
-def check_result(msg):
-    print "TEST_SUCCESS", msg
 
 def make_zero_image(*args):
     for i in args:
