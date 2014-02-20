872ded6c9a206508e85a4e3993ba38743b3b7355
MITSUNARI Shigeo <herumi@nifty.com>
2014-02-20 11:26:53 +0900

    add StateMap

---
+++ src/state_map.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <mutex>
+#include "cybozu/exception.hpp"
+
+namespace walb {
+
+template<class State>
+class StateMap {
+    std::mutex mu_;
+    using Map = std::map<std::string, std::unique_ptr<State>>;
+    using AutoLock = std::lock_guard<std::mutex>;
+    Map map_;
+public:
+    std::pair<State *, bool> get(const std::string& volId)
+    {
+        AutoLock al(mu_);
+        bool maked;
+        typename Map::iterator itr;
+        std::tie(itr, maked) = map_.insert(std::make_pair(volId, nullptr));
+        if (!maked) {
+            itr->second = std::unique_ptr<State>(new State());
+        }
+        return std::make_pair(itr->second.get(), maked);
+    }
+    void del(const std::string& volId)
+    {
+        AutoLock al(mu_);
+        typename Map::iterator i = map_.find(volId);
+        if (i == map_.end()) throw cybozu::Exception("StateMap:del:not found") << volId;
+        map_.erase(i);
+    }
+};
+
+} // walb
+
---
+++ utest/state_map_test.cpp
@@ -0,0 +1,48 @@
+#include "cybozu/test.hpp"
+#include "state_map.hpp"
+
+struct State {
+    int a;
+    State() : a(5) {}
+};
+
+CYBOZU_TEST_AUTO(StateMap)
+{
+    walb::StateMap<State> m;
+    {
+        State *p = 0;
+        bool maked = false;
+        std::tie(p, maked) = m.get("abc");
+        CYBOZU_TEST_ASSERT(maked);
+        CYBOZU_TEST_ASSERT(p);
+        CYBOZU_TEST_EQUAL(p->a, 5);
+    }
+    {
+        State *p = 0;
+        bool maked = false;
+        std::tie(p, maked) = m.get("abc");
+        CYBOZU_TEST_ASSERT(!maked);
+        CYBOZU_TEST_ASSERT(p);
+        CYBOZU_TEST_EQUAL(p->a, 5);
+        p->a = 9;
+    }
+    {
+        State *p = 0;
+        bool maked = false;
+        std::tie(p, maked) = m.get("abc");
+        CYBOZU_TEST_ASSERT(!maked);
+        CYBOZU_TEST_ASSERT(p);
+        CYBOZU_TEST_EQUAL(p->a, 9);
+    }
+    m.del("abc");
+    {
+        State *p = 0;
+        bool maked = false;
+        std::tie(p, maked) = m.get("abc");
+        CYBOZU_TEST_ASSERT(maked);
+        CYBOZU_TEST_ASSERT(p);
+        CYBOZU_TEST_EQUAL(p->a, 5);
+    }
+    CYBOZU_TEST_EXCEPTION(m.del("abc"), cybozu::Exception);
+}
+
