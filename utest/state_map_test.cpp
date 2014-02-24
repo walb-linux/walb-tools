f555b46aca46336a124fb07b95aa198eb90ddad0
MITSUNARI Shigeo <herumi@nifty.com>
2014-02-24 16:25:01 +0900

    test of StateMap

--- utest/state_map_test.cpp
+++ utest/state_map_test.cpp
@@ -6,47 +6,36 @@ struct State {
     State() : a(5) {}
 };
 
-#if 0
-QQQ
 CYBOZU_TEST_AUTO(StateMap)
 {
     walb::StateMap<State> m;
     {
         bool maked = false;
-        State &p = 0;
-        std::tie(p, maked) = m.get("abc");
+        State& s = m.get("abc", &maked);
         CYBOZU_TEST_ASSERT(maked);
-        CYBOZU_TEST_ASSERT(p);
-        CYBOZU_TEST_EQUAL(p->a, 5);
+        CYBOZU_TEST_EQUAL(s.a, 5);
     }
     {
-        State *p = 0;
         bool maked = false;
-        std::tie(p, maked) = m.get("abc");
+        State& s = m.get("abc", &maked);
         CYBOZU_TEST_ASSERT(!maked);
-        CYBOZU_TEST_ASSERT(p);
-        CYBOZU_TEST_EQUAL(p->a, 5);
-        p->a = 9;
+        CYBOZU_TEST_EQUAL(s.a, 5);
+        s.a = 9;
     }
     {
-        State *p = 0;
         bool maked = false;
-        std::tie(p, maked) = m.get("abc");
+        State& s = m.get("abc", &maked);
         CYBOZU_TEST_ASSERT(!maked);
-        CYBOZU_TEST_ASSERT(p);
-        CYBOZU_TEST_EQUAL(p->a, 9);
+        CYBOZU_TEST_EQUAL(s.a, 9);
     }
     m.del("abc");
     {
-        State *p = 0;
         bool maked = false;
-        std::tie(p, maked) = m.get("abc");
+        State& s = m.get("abc", &maked);
         CYBOZU_TEST_ASSERT(maked);
-        CYBOZU_TEST_ASSERT(p);
-        CYBOZU_TEST_EQUAL(p->a, 5);
+        CYBOZU_TEST_EQUAL(s.a, 5);
     }
     m.del("abc");
     CYBOZU_TEST_EXCEPTION(m.del("abc"), cybozu::Exception);
 }
-#endif
 
