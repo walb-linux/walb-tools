effc10f43676b8700e2453b5b616e04dcbe65bbe
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-03-26 15:00:58 +0900

    change values of several constants.

--- src/constant.hpp
+++ src/constant.hpp
@@ -16,10 +16,10 @@ const size_t DEFAULT_MAX_BACKGROUND_TASKS = 1;
 const size_t DEFAULT_MAX_WDIFF_SEND_MB = 128;
 const size_t DEFAULT_MAX_WLOG_SEND_MB = 128;
 const size_t DEFAULT_MAX_CONVERSION_MB = 1024;
-const size_t DEFAULT_DELAY_SEC_FOR_RETRY = 60; // seconds.
+const size_t DEFAULT_DELAY_SEC_FOR_RETRY = 20; // seconds.
 const size_t DEFAULT_RETRY_TIMEOUT = 1800; // seconds.
 
-const size_t PROXY_HEARTBEAT_INTERVAL_SEC = 60;
+const size_t PROXY_HEARTBEAT_INTERVAL_SEC = 10;
 
 const size_t DEFAULT_MAX_IO_LB = MEBI / LBS; // used as max diff IO size.
 
