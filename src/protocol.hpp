753b0121d5986ce90b61a41309a9b039134bae1f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-04-01 23:03:50 +0900

    add StopType, StopOpt, and parseStopParams().

--- src/protocol.hpp
+++ src/protocol.hpp
@@ -85,6 +85,52 @@ inline std::vector<cybozu::SocketAddr> parseMultiSocketAddr(const std::string &m
     return ret;
 }
 
+enum class StopType
+{
+    Graceful, Empty, Force,
+};
+
+struct StopOpt
+{
+    StopType type;
+
+    StopOpt() : type(StopType::Graceful) {
+    }
+    std::string str() const {
+        switch (type) {
+        case StopType::Graceful: return "graceful";
+        case StopType::Empty: return "empty";
+        case StopType::Force: return "force";
+        }
+        throw cybozu::Exception(__func__) << "bug" << int(type);
+    }
+    void parse(const std::string &s) {
+        type = StopType::Graceful;
+        if (s.empty()) return;
+        if (s[0] == 'f') {
+            type = StopType::Force;
+        } else if (s[0] == 'e') {
+            type = StopType::Empty;
+        }
+    }
+    bool isForce() const { return type == StopType::Force; }
+    bool isGraceful() const { return type == StopType::Graceful; }
+    bool isEmpty() const { return type == StopType::Empty; }
+    friend inline std::ostream &operator<<(std::ostream &os, const StopOpt &opt) {
+        os << opt.str();
+        return os;
+    }
+};
+
+inline std::pair<std::string, StopOpt> parseStopParams(const StrVec &v, const char *msg)
+{
+    if (v.empty()) throw cybozu::Exception(msg) << "empty";
+    if (v[0].empty()) throw cybozu::Exception(msg) << "empty volId";
+    StopOpt opt;
+    if (v.size() >= 2) opt.parse(v[1]);
+    return {v[0], opt};
+}
+
 namespace protocol {
 
 /**
