2dc39433038a2a7d85bc8b9bc526f18735c69218
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-07-14 19:39:20 +0900

    write_overlapped_and_verify: more precise verification.

--- binsrc/write_overlapped_and_verify.cpp
+++ binsrc/write_overlapped_and_verify.cpp
@@ -12,6 +12,7 @@
 #include "walb/common.h"
 #include "walb/block_size.h"
 #include "walb_util.hpp"
+#include "checksum.hpp"
 
 using namespace walb;
 
@@ -22,8 +23,9 @@ struct Option
     uint64_t size; /* [byte]. */
     uint32_t minIoSize; /* [byte]. */
     uint32_t maxIoSize; /* [byte]. */
-    uint32_t counts; /* The number of write IOs per thread. */
+    uint32_t periodS; /* running period [sec]. */
     uint32_t numThreads; /* The number of threads. */
+    bool isVerbose;
     bool isDebug;
     std::string targetPath; /* device or file path. */
 
@@ -32,11 +34,12 @@ struct Option
         opt.setDescription("write_overlapped_and_verify: issud overlapped write IOs and verify the written data.");
         opt.appendOpt(&bs, LOGICAL_BLOCK_SIZE, "b", cybozu::format("SIZE:  block size [byte]. (default: %u)", LOGICAL_BLOCK_SIZE));
         opt.appendOpt(&offset, 0, "o", "OFFSET: start offset [byte]. (default: 0)");
-        opt.appendOpt(&size, 0, "s", "SIZE: target size [byte]. (default: device size)");
+        opt.appendOpt(&size, MEBI, "s", "SIZE: target size [byte]. (default: 1M)");
         opt.appendOpt(&minIoSize, LOGICAL_BLOCK_SIZE, "n", cybozu::format("SIZE: minimum IO size [byte]. (default: %u)", LOGICAL_BLOCK_SIZE));
         opt.appendOpt(&maxIoSize, LOGICAL_BLOCK_SIZE * 64, "x", cybozu::format("SIZE: maximum IO size [byte]. (default: %u)", LOGICAL_BLOCK_SIZE * 64));
-        opt.appendOpt(&counts, 100, "c", "N: number of write IOs per threads (default: 100)");
+        opt.appendOpt(&periodS, 1, "p", "N: running period [sec] (default: 1)");
         opt.appendOpt(&numThreads, 2, "t", "N: number of threads (default: 2)");
+        opt.appendBoolOpt(&isVerbose, "v", ": put verbose messages to stderr.");
         opt.appendBoolOpt(&isDebug, "debug", ": put debug messages to stderr.");
         opt.appendHelp("h", ": show this message.");
         opt.appendParam(&targetPath, "[DEVICE|FILE]");
@@ -79,8 +82,11 @@ private:
             throw cybozu::Exception(__func__)
                 << "minIoSize must be <= maxIoSize" << minIoB() << maxIoB();
         }
-        if (counts == 0) {
-            throw RT_ERR("counts must be > 0.");
+        if (size == 0) {
+            throw RT_ERR("size must be > 0.");
+        }
+        if (periodS == 0) {
+            throw RT_ERR("periodS must be > 0.");
         }
         if (numThreads == 0) {
             throw RT_ERR("numThreads must be > 0.");
@@ -119,38 +125,137 @@ public:
     }
 };
 
+class RangeMutex
+{
+private:
+    const size_t size_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+
+    using Bmp = std::vector<bool>;
+    Bmp bmp_;
+
+public:
+    explicit RangeMutex(size_t size)
+        : size_(size), mutex_(), cv_(), bmp_(size) {
+    }
+    void lock(size_t idx, size_t size) noexcept {
+        assert(idx + size <= size_);
+        Bmp::iterator bgn = bmp_.begin() + idx, end = bgn + size;
+
+        std::unique_lock<std::mutex> lk(mutex_);
+        while (std::any_of(bgn, end, [](bool b) { return b; })) {
+            cv_.wait(lk);
+        }
+        std::fill(bgn, end, true);
+    }
+    void unlock(size_t idx, size_t size) noexcept {
+        assert(idx + size <= size_);
+        Bmp::iterator bgn = bmp_.begin() + idx, end = bgn + size;
+
+        std::unique_lock<std::mutex> lk(mutex_);
+        assert(std::all_of(bgn, end, [](bool b) { return b; }));
+        std::fill(bgn, end, false);
+        cv_.notify_all();
+    }
+};
+
+class RangeLock
+{
+private:
+    RangeMutex *muP_;
+    size_t idx_;
+    size_t size_;
+    bool locked_;
+
+public:
+    RangeLock()
+        : muP_(nullptr), idx_(-1), size_(-1), locked_(false) {
+    }
+    explicit RangeLock(RangeMutex &mu, size_t idx, size_t size) : RangeLock() {
+        muP_ = &mu;
+        idx_ = idx;
+        size_ = size;
+        lock();
+    }
+    RangeLock(RangeLock&& rhs) : RangeLock() {
+        swap(rhs);
+    }
+    RangeLock& operator=(RangeLock&& rhs) {
+        unlock();
+        swap(rhs);
+        return *this;
+    }
+    void swap(RangeLock& rhs) {
+        std::swap(muP_, rhs.muP_);
+        std::swap(idx_, rhs.idx_);
+        std::swap(size_, rhs.size_);
+        std::swap(locked_, rhs.locked_);
+    }
+    void lock() noexcept {
+        assert(muP_);
+        assert(!locked_);
+        muP_->lock(idx_, size_);
+        locked_ = true;
+    }
+    void unlock() noexcept {
+        assert(muP_);
+        if (!locked_) return;
+        muP_->unlock(idx_, size_);
+        locked_ = false;
+    }
+    ~RangeLock() noexcept {
+        unlock();
+    }
+};
+
 class Worker
 {
 private:
     const Option &opt_;
     cybozu::util::File file_;
     RandomIoSpecGenerator ioSpecGen_;
-    const char * const dataPtr_;
-    std::vector<bool> bmp_;
+    const char *dataPtr_;
+    std::vector<uint64_t> ioIdV_;
+    RangeMutex &mu_;
+    std::atomic<uint64_t> &ioIdGen_;
+    std::atomic<bool> &shouldStop_;
 
 public:
-    Worker(const Option &opt, const char *dataPtr)
+    Worker(const Option &opt, const char *dataPtr, RangeMutex &mu, std::atomic<uint64_t> &ioIdGen,
+           std::atomic<bool> &shouldStop)
         : opt_(opt)
         , file_(opt.targetPath, O_RDWR | O_DIRECT)
         , ioSpecGen_(opt.offsetB(), opt.sizeB())
         , dataPtr_(dataPtr)
-        , bmp_(opt.sizeB(), false) {}
-
+        , ioIdV_(opt.sizeB(), 0)
+        , mu_(mu)
+        , ioIdGen_(ioIdGen)
+        , shouldStop_(shouldStop) {
+    }
     void operator()() {
         const uint32_t bs = opt_.bs;
         uint64_t ioAddr;
         uint32_t ioBlocks;
-        for (size_t i = 0; i < opt_.counts; i++) {
+        while (!shouldStop_) {
             ioSpecGen_.get(ioAddr, ioBlocks);
+
+            RangeLock lk(mu_, ioAddr, ioBlocks);
+            const uint64_t ioId = ioIdGen_++;
             const uint64_t offLb = (ioAddr - opt_.offsetB());
             file_.pwrite(dataPtr_ + (offLb * bs), ioBlocks * bs, ioAddr * bs);
-            for (uint64_t i = offLb; i < offLb + ioBlocks; i++) {
-                bmp_[i] = true;
-            }
+            fillIoIdV(offLb, ioBlocks, ioId);
         }
     }
-    const std::vector<bool> &getBmp() const {
-        return bmp_;
+    const std::vector<uint64_t> &getIoIdV() const {
+        return ioIdV_;
+    }
+private:
+    void fillIoIdV(uint64_t offLb, uint32_t ioBlocks, uint64_t ioId) {
+        std::vector<uint64_t>::iterator bgn, end;
+        bgn = ioIdV_.begin() + offLb;
+        end = bgn + ioBlocks;
+        std::fill(bgn, end, ioId);
     }
 };
 
@@ -161,10 +266,11 @@ void writeConcurrentlyAndVerify(Option &opt)
     cybozu::util::File file(opt.targetPath, O_RDWR | O_DIRECT);
 
     /* Decide target area size. */
-    size_t sizeB = opt.sizeB();
+    const size_t sizeB = opt.sizeB();
     const size_t devSizeB = cybozu::util::getBlockDeviceSize(file.fd()) / bs;
-    if (sizeB == 0 || devSizeB <= opt.offsetB() + sizeB) {
-        sizeB = devSizeB - opt.offsetB();
+    if (devSizeB < opt.offsetB() + sizeB) {
+        throw cybozu::Exception(__func__)
+            << "specified area is out of range" << devSizeB << opt.offsetB() << sizeB;
     }
 
     /* Write zero to the target range. */
@@ -174,38 +280,71 @@ void writeConcurrentlyAndVerify(Option &opt)
         file.write(blk.data(), blk.size());
     }
 
-    /* Prepare shared blocks and fill randomly. */
-    cybozu::util::Random<uint64_t> rand;
-    AlignedArray blks(sizeB * bs);
-    rand.fill(blk.data(), blk.size());
+
+    /* Prepare resources shared by all threads. */
+    std::atomic<uint64_t> ioIdGen(1); // 0 means invalid.
+    RangeMutex mu(sizeB);
+    std::atomic<bool> shouldStop(false);
 
     /* Prepare writer threads and run concurrently. */
-    std::vector<std::shared_ptr<Worker>> v;
+    /* Prepare blocks and fill randomly. */
+    cybozu::util::Random<uint64_t> rand;
+    std::vector<AlignedArray> blksV;
+    std::vector<std::shared_ptr<Worker> > thV;
     for (size_t i = 0; i < opt.numThreads; i++) {
-        v.push_back(std::make_shared<Worker>(opt, blks.data()));
+        blksV.emplace_back(opt.size);
+        rand.fill(blksV.back().data(), opt.size);
+        thV.push_back(std::make_shared<Worker>(
+                          opt, blksV.back().data(), mu, ioIdGen, shouldStop));
+    }
+    if (opt.isVerbose) {
+        for (size_t thId = 0; thId < opt.numThreads; thId++) {
+            ::printf("%zu", thId);
+            for (size_t i = 0; i < sizeB; i++) {
+                const char *data = blksV[thId].data() + (i * bs);
+                ::printf(" %08x", cybozu::util::calcChecksum(data, bs, 0));
+            }
+            ::printf("\n");
+        }
     }
     cybozu::thread::ThreadRunnerSet thSet;
-    for (std::shared_ptr<Worker> &w : v) {
+    for (std::shared_ptr<Worker> &w : thV) {
         thSet.add(w);
     }
     thSet.start();
+    util::sleepMs(opt.periodS * 1000);
+    shouldStop = true;
     thSet.join();
 
-    /* execute OR for each bit. */
-    std::vector<bool> bmp(sizeB, false);
+    /* Determine who writes each block finally. */
+    std::vector<size_t> threadIdV(sizeB, -1); // -1 means no one writes.
     for (size_t i = 0; i < sizeB; i++) {
-        for (std::shared_ptr<Worker> &w : v) {
-            if (w->getBmp()[i]) { bmp[i] = true; break; }
+        uint64_t maxIoId = 0;
+        size_t thId = 0;
+        for (size_t j = 0; j < opt.numThreads; j++) {
+            const uint64_t ioId = thV[j]->getIoIdV()[i];
+            if (maxIoId < ioId) {
+                maxIoId = ioId;
+                thId = j;
+            }
+        }
+        if (maxIoId > 0) {
+            threadIdV[i] = thId;
         }
     }
 
     /* Verify. */
     uint64_t nVerified = 0, nWritten = 0;
     for (uint64_t i = 0; i < sizeB; i++) {
-        if (!bmp[i]) continue;
+        const size_t thId = threadIdV[i];
+        if (thId == uint64_t(-1)) continue;
         nWritten++;
         file.pread(blk.data(), bs, (opt.offsetB() + i) * bs);
-        if (::memcmp(blk.data(), blks.data() + (i * bs), bs) == 0) {
+        if (opt.isVerbose) {
+            const uint32_t csum = cybozu::util::calcChecksum(blk.data(), blk.size(), 0);
+            ::printf("%zu %08x\n", thId, csum);
+        }
+        if (::memcmp(blk.data(), blksV[thId].data() + (i * bs), bs) == 0) {
             nVerified++;
         } else {
             ::printf("block %" PRIu64 " invalid.\n", i);
