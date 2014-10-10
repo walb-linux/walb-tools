12c8db69beef66f93dfa2d52215e8827c4b5faa7
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-10-10 18:11:52 +0900

    bdiff: add -d option to show difference.

--- binsrc/bdiff.cpp
+++ binsrc/bdiff.cpp
@@ -24,12 +24,14 @@ private:
     std::string file2_;
     size_t lineSize_;
 public:
+    bool doShowDiff;
     Config(int argc, char* argv[]) {
         cybozu::Option opt;
         opt.setDescription("bdiff: Show block diff.");
         opt.appendOpt(&blockSize_, 512, "b", "SIZE: block size in bytes (default: 512)");
         opt.appendBoolOpt(&isVerbose_, "v", ": verbose messages to stderr.");
-        opt.appendOpt(&lineSize_, 64, "l", ": line size in printing invalid block contents.");
+        opt.appendOpt(&lineSize_, 32, "l", ": line size in printing different block contents.");
+        opt.appendBoolOpt(&doShowDiff, "d", ": show diff for different contents.");
         opt.appendHelp("h", ": show this message.");
         opt.appendParam(&file1_, "FILE1");
         opt.appendParam(&file2_, "FILE2");
@@ -52,6 +54,24 @@ public:
         }
     }
 };
+
+void showDifference(const void *data0, const void *data1, size_t s, size_t lineSize)
+{
+    const char *p0 = (const char *)data0;
+    const char *p1 = (const char *)data1;
+
+    for (size_t i = 0; i < s; i++) {
+        ::printf("%s", (*p0 == *p1 ? "_" : "X"));
+        p0++;
+        p1++;
+        if (i % lineSize == lineSize - 1) {
+            ::printf("\n");
+        }
+    }
+    if (s % lineSize != 0) {
+        ::printf("\n");
+    }
+}
 
 /**
  * RETURN:
@@ -78,8 +98,12 @@ uint64_t checkBlockDiff(Config& config)
                 nDiffer++;
                 if (config.isVerbose()) {
                     ::printf("block %" PRIu64 " differ\n", nChecked);
-                    cybozu::util::printByteArray(a1.data(), bs, config.lineSize());
-                    cybozu::util::printByteArray(a2.data(), bs, config.lineSize());
+                    const size_t lineS = config.lineSize();
+                    cybozu::util::printByteArray(a1.data(), bs, lineS);
+                    cybozu::util::printByteArray(a2.data(), bs, lineS);
+                    if (config.doShowDiff) {
+                        showDifference(a1.data(), a2.data(), bs, lineS);
+                    }
                 }
             }
             nChecked++;
