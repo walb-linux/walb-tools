df31017142ce3b302b368dbf9c58db3aa6b84e37
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-10-21 19:41:04 +0900

    fill-bdev: change option -p to -rp and add option -dp.

--- binsrc/fill-bdev.cpp
+++ binsrc/fill-bdev.cpp
@@ -8,6 +8,7 @@
 #include "bdev_util.hpp"
 #include "walb_util.hpp"
 #include "walb_types.hpp"
+#include "constant.hpp"
 
 using namespace walb;
 
@@ -16,17 +17,19 @@ struct Option
     size_t bs;
     uint64_t offset;
     uint64_t size;
-    size_t permillage;
+    size_t diffPml;
+    size_t randPml;
     bool isDebug;
     std::string bdevPath;
 
     Option(int argc, char* argv[]) {
         cybozu::Option opt;
         opt.setDescription("Fill a block device or file randomly.\n");
-        opt.appendOpt(&bs, 64 << 10, "b", "SIZE: block size [byte] (default: 64K).");
         opt.appendOpt(&offset, 0, "o", "OFFSET: offset to start filling [byte].");
-        opt.appendOpt(&size, 0, "s", "SIZE: filling size [byte].");
-        opt.appendOpt(&permillage, 0, "p", "PERMILLAGE: random data permillage [0, 1000].");
+        opt.appendOpt(&size, 0, "s", "SIZE: filling target size [byte].");
+        opt.appendOpt(&bs, 64 << 10, "b", "SIZE: block size [byte] (default: 64K).");
+        opt.appendOpt(&diffPml, 10, "dp", "PERMILLAGE: what permillage of blocks will be written [0,1000].");
+        opt.appendOpt(&randPml, 0, "rp", "PERMILLAGE: random data permillage in each written block [0, 1000].");
         opt.appendBoolOpt(&isDebug, "debug", ": put debug messages to stderr.");
         opt.appendParam(&bdevPath, "DEVICE_PATH");
         opt.appendHelp("h", ": show this message.");
@@ -36,8 +39,11 @@ struct Option
             ::exit(1);
         }
 
-        if (permillage > 1000) {
-            throw cybozu::Exception("bad permillage") << permillage;
+        if (diffPml > 1000) {
+            throw cybozu::Exception("bad permillage for -dp") << diffPml;
+        }
+        if (randPml > 1000) {
+            throw cybozu::Exception("bad permillage for -rp") << randPml;
         }
         if (bs == 0 || bs > (32 << 20) || bs % 512 != 0) {
             throw cybozu::Exception("bad block size") << bs;
@@ -80,8 +86,8 @@ private:
     }
 };
 
-void diffuse(cybozu::util::Random<size_t>& rand,
-             const AlignedArray& src, AlignedArray& dst)
+template <typename Rand>
+void diffuse(Rand& rand, const AlignedArray& src, AlignedArray& dst)
 {
     assert(src.size() <= dst.size());
     size_t nrSpace = dst.size() - src.size();
@@ -109,30 +115,53 @@ int doMain(int argc, char* argv[])
             << opt.offset << opt.size << bdevSize;
     }
     file.lseek(opt.offset);
-    const size_t fillSize = opt.bs * opt.permillage / 1000;
-    assert(fillSize <= opt.bs);
+    const size_t bs = opt.bs;
+    const size_t fillSize = bs * opt.randPml / 1000;
+    assert(fillSize <= bs);
     AlignedArray rbuf(fillSize);
     uint64_t size = opt.size == 0 ? (bdevSize - opt.offset) : opt.size;
     assert(size % 512 == 0);
-    const uint64_t rem = size % opt.bs;
+    const uint64_t rem = size % bs;
     if (rem != 0) size -= rem;
-    assert(size % opt.bs == 0);
-    AlignedArray wbuf(opt.bs);
+    assert(size % bs == 0);
+    AlignedArray wbuf(bs);
+#if 0
     cybozu::util::Random<size_t> rand;
+#else
+    cybozu::util::XorShift128 rand(::time(0));
+#endif
 
     ProgressPrinter pp(::stderr);
+    uint64_t nrUpdated = 0, nrSkipped = 0;
     uint64_t remaining = size;
+    uint64_t written = 0;
     while (remaining > 0) {
-        rand.fill(rbuf.data(), rbuf.size());
-        ::memset(wbuf.data(), 0, wbuf.size());
-        diffuse(rand, rbuf, wbuf);
-        file.write(wbuf.data(), wbuf.size());
-        remaining -= wbuf.size();
-        pp.progress(wbuf.size());
+        if (rand() % 1000 < opt.diffPml) {
+            rand.fill(rbuf.data(), rbuf.size());
+            ::memset(wbuf.data(), 0, bs);
+            diffuse(rand, rbuf, wbuf);
+            file.write(wbuf.data(), bs);
+            nrUpdated++;
+            written += bs;
+        } else {
+            file.lseek(bs, SEEK_CUR);
+            nrSkipped++;
+        }
+        remaining -= bs;
+        pp.progress(bs);
+        if (written >= MAX_FSYNC_DATA_SIZE) {
+            file.fdatasync();
+            written = 0;
+        }
     }
     pp.end();
     file.fdatasync();
     file.close();
+    ::printf("updated blocks: %" PRIu64 "\n"
+             "skipped blocks: %" PRIu64 "\n"
+             "update permill: %" PRIu64 "\n"
+             , nrUpdated, nrSkipped
+             , nrUpdated * 1000 / (nrUpdated + nrSkipped));
     return 0;
 }
 
