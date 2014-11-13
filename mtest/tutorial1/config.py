1849c0eba4f626b913f3f1a10e52a7e0864bac74
MITSUNARI Shigeo <herumi@nifty.com>
2014-11-13 13:20:38 +0900

    add tutorial1 config

---
+++ mtest/tutorial1/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../../python/walb/')
+from walb import *
+
+binDir = '../../binsrc/'
+wdevcPath = binDir + 'wdevc'
+walbcPath = binDir + 'walbc'
+
+def dataPath(s):
+    return '/mnt/tutorial/data/%s/' % s
+
+def logPath(s):
+    return '/mnt/tutorial/data/%s.log' % s
+
+s0 = Server('s0', 'localhost', 10000, K_STORAGE, binDir, dataPath('s0'), logPath('s0'))
+p0 = Server('p0', 'localhost', 10100, K_PROXY, binDir, dataPath('p0'), logPath('p0'))
+a0 = Server('a0', 'localhost', 10200, K_ARCHIVE, binDir, dataPath('a0'), logPath('a0'), 'tutorial')
+
+sLayout = ServerLayout([s0], [p0], [a0])
+walbc = Controller(walbcPath, sLayout, isDebug=False)
+
+runCommand = walbc.get_run_remote_command(s0)
+wdev0 = Device(0, '/dev/tutorial/wlog', '/dev/tutorial/wdata', wdevcPath, runCommand)
+
+VOL = 'vol'
