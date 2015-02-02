eab8d2a8eac03b78f5dd74752609711e224c6be9
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2015-02-02 17:32:33 +0900

    use another thread for pop and send packs in wdiff transfer.

--- src/wdiff_transfer.hpp
+++ src/wdiff_transfer.hpp
@@ -18,22 +18,29 @@ inline bool wdiffTransferClient(
     const std::atomic<int> &stopState, const ProcessStatus &ps,
     DiffStatistics &statOut)
 {
-    statOut.clear();
-    statOut.wdiffNr = -1;
-    packet::StreamControl ctrl(pkt.sock());
-
-    auto sendPack = [&](const Buffer& pack) {
-        ctrl.next();
-        pkt.write<size_t>(pack.size());
-        pkt.write(pack.data(), pack.size());
-        statOut.update(*(const DiffPackHeader*)pack.data());
-    };
-
-    DiffRecIo recIo;
     const size_t maxPushedNum = cmpr.numCpu * 2 - 1;
     ConverterQueue conv(maxPushedNum, cmpr.numCpu, true, cmpr.type, cmpr.level);
+
+    auto sendPack = [&]() {
+        statOut.clear();
+        statOut.wdiffNr = -1;
+        packet::StreamControl ctrl(pkt.sock());
+
+        Buffer pack = conv.pop();
+        while (!pack.empty()) {
+            ctrl.next();
+            pkt.write<size_t>(pack.size());
+            pkt.write(pack.data(), pack.size());
+            statOut.update(*(const DiffPackHeader*)pack.data());
+            pack = conv.pop();
+        }
+        ctrl.end();
+    };
+    cybozu::thread::ThreadRunner senderTh(sendPack);
+    senderTh.start();
+
+    DiffRecIo recIo;
     DiffPacker packer;
-    size_t pushedNum = 0;
     while (merger.getAndRemove(recIo)) {
         if (stopState == ForceStopping || ps.isForceShutdown()) {
             return false;
@@ -44,25 +51,14 @@ inline bool wdiffTransferClient(
             continue;
         }
         conv.push(packer.getPackAsVector());
-        pushedNum++;
         packer.reset();
         packer.add(rec, io.get());
-        if (pushedNum < maxPushedNum) {
-            continue;
-        }
-        sendPack(conv.pop());
-        pushedNum--;
     }
     if (!packer.empty()) {
         conv.push(packer.getPackAsVector());
     }
     conv.quit();
-    Buffer pack = conv.pop();
-    while (!pack.empty()) {
-        sendPack(pack);
-        pack = conv.pop();
-    }
-    ctrl.end();
+    senderTh.join();
     return true;
 }
 
