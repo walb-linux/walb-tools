ff406e0298cd9d5d1ead9a96c2ad7bd8f0796763
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2015-02-12 15:30:53 +0900

    remove obsolute code.

--- src/walb_diff_compressor.hpp
+++ src/walb_diff_compressor.hpp
@@ -161,262 +161,13 @@ public:
 
 namespace compressor_local {
 
-typedef std::pair<compressor::Buffer, std::exception_ptr> MaybeBuffer;
-
-class Queue {
-    const std::atomic<bool> *pq_;
-    size_t maxQueSize_;
-    std::queue<MaybeBuffer> q_;
-    mutable std::mutex m_;
-    std::condition_variable avail_;
-    std::condition_variable notFull_;
-
-    bool isAvailable(const MaybeBuffer &buf) const {
-        return !buf.first.empty() || buf.second;
-    }
-public:
-    explicit Queue(const std::atomic<bool> *pq, size_t maxQueSize) : pq_(pq), maxQueSize_(maxQueSize) {}
-    /*
-        allocate reserved buffer where will be stored laster and return it
-        @note lock if queue is fill
-    */
-    MaybeBuffer *push()
-    {
-        std::unique_lock<std::mutex> lk(m_);
-        notFull_.wait(lk, [this] { return q_.size() < maxQueSize_; });
-        q_.push(MaybeBuffer());
-        return &q_.back();
-    }
-    /*
-        notify to pop() that buffer is released
-    */
-    void notify()
-    {
-        std::unique_lock<std::mutex> lk(m_);
-        avail_.notify_one();
-    }
-    compressor::Buffer pop()
-    {
-        MaybeBuffer ret;
-        {
-            std::unique_lock<std::mutex> lk(m_);
-            avail_.wait(lk, [this] {
-                return (!q_.empty() && isAvailable(q_.front())) || (*pq_ && q_.empty());
-            });
-            if (*pq_ && q_.empty()) return compressor::Buffer();
-            ret = std::move(q_.front());
-            q_.pop();
-            notFull_.notify_one();
-        }
-        if (ret.second) std::rethrow_exception(ret.second);
-        return std::move(ret.first);
-    }
-    bool empty() const
-    {
-        std::lock_guard<std::mutex> lk(m_);
-        return q_.empty();
-    }
-};
-
-template<class Conv, class UnConv>
-struct EngineT : cybozu::ThreadBase {
-private:
-    compressor::PackCompressorBase *e_;
-    std::atomic<bool> using_;
-    Queue *que_;
-    cybozu::Event startEv_;
-    cybozu::Event *endEv_;
-    compressor::Buffer inBuf_;
-    MaybeBuffer *outBuf_;
-    EngineT(const EngineT&) = delete;
-    void operator=(const EngineT&) = delete;
-public:
-    EngineT()
-        : e_(nullptr)
-        , using_(false)
-        , que_(nullptr)
-        , endEv_(nullptr)
-        , outBuf_(nullptr)
-    {
-    }
-    ~EngineT()
-    {
-        delete e_;
-    }
-    void init(bool doCompress, int type, size_t para, Queue *que, cybozu::Event *endEv)
-    {
-        assert(e_ == nullptr);
-        if (doCompress) {
-            e_ = new Conv(type, para);
-        } else {
-            e_ = new UnConv(type, para);
-        }
-        que_ = que;
-        endEv_ = endEv;
-        beginThread();
-    }
-    void threadEntry()
-        try
-    {
-        assert(e_);
-        for (;;) {
-            startEv_.wait();
-            /*
-             * case 1 (process task): set inBuf_ and outBuf_ and wakeup().
-             * case 2 (quit): just call wakeup() without setting inBuf_ and outBuf_.
-             */
-            if (inBuf_.empty()) {
-                que_->notify();
-                break;
-            }
-            assert(outBuf_);
-            try {
-                outBuf_->first = e_->convert(inBuf_.data());
-            } catch (...) {
-                outBuf_->second = std::current_exception();
-            }
-            inBuf_.clear();
-            outBuf_ = nullptr;
-            using_ = false;
-            endEv_->wakeup();
-            que_->notify();
-        }
-    } catch (std::exception& e) {
-        printf("compress_local::Engine::threadEntry %s\n", e.what());
-    }
-    void wakeup()
-    {
-        startEv_.wakeup();
-    }
-    bool tryToRun(MaybeBuffer *outBuf, compressor::Buffer&& inBuf)
-    {
-        if (using_.exchange(true)) return false;
-        inBuf_ = std::move(inBuf);
-        outBuf_ = outBuf;
-        startEv_.wakeup();
-        return true;
-    }
-    bool isUsing() const { return using_; }
-};
-
-template<class Conv = PackCompressor, class UnConv = PackUncompressor>
-class ConverterQueueT {
-    typedef EngineT<Conv, UnConv> Engine;
-    std::atomic<bool> quit_;
-    std::atomic<bool> joined_;
-    Queue que_;
-    cybozu::Event endEv_;
-    std::vector<Engine> enginePool_;
-    void runEngine(MaybeBuffer *outBuf, compressor::Buffer&& inBuf)
-    {
-        for (;;) {
-            for (Engine& e : enginePool_) {
-                if (e.tryToRun(outBuf, std::move(inBuf))) return;
-            }
-            endEv_.wait();
-        }
-    }
-    bool isFreeEngine() const
-    {
-        for (const Engine& e : enginePool_) {
-            if (e.isUsing()) return false;
-        }
-        return true;
-    }
-    void Sleep1msec() const
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-public:
-    ConverterQueueT(size_t maxQueueNum, size_t threadNum, bool doCompress, int type, size_t para = 0)
-        : quit_(false)
-        , joined_(false)
-        , que_(&quit_, maxQueueNum)
-        , enginePool_(threadNum)
-    {
-        for (Engine& e : enginePool_) {
-            e.init(doCompress, type, para, &que_, &endEv_);
-        }
-    }
-    ~ConverterQueueT()
-        try
-    {
-        join();
-    } catch (std::exception& e) {
-        printf("ConverterQueue:dstr:%s\n", e.what());
-        exit(1);
-    } catch (...) {
-        printf("ConverterQueue:dstr:unknown exception\n");
-        exit(1);
-    }
-    /*
-     * join all thread
-     */
-    void join()
-    {
-        if (joined_.exchange(true)) return;
-        quit();
-        while (!isFreeEngine()) {
-            Sleep1msec();
-        }
-        while (!que_.empty()) {
-            Sleep1msec();
-        }
-        for (Engine& e : enginePool_) {
-            e.wakeup();
-            e.joinThread();
-        }
-    }
-    /*
-     * start to quit all thread
-     */
-    void quit() {
-        quit_ = true;
-        que_.notify();
-    }
-    /*
-     * push buffer and return true
-     * return false if quit_
-     * @param inBuf [in] not empty
-     */
-    bool push(compressor::Buffer&& inBuf)
-    {
-        if (inBuf.empty()) throw cybozu::Exception("walb:ConverterQueueT:push:inBuf is empty");
-        if (quit_) return false;
-        MaybeBuffer *outBuf = que_.push();
-        runEngine(outBuf, std::move(inBuf)); // no throw
-        return true;
-    }
-    /*
-     * return empty buffer if quit_ and queue is empty
-     * otherwise return buffer after blocking until data comes
-     */
-    compressor::Buffer pop()
-    {
-        return que_.pop();
-    }
-    /*
-     * You must call this to notify the thread calling push()
-     * because it may not detect that quit variable becomes true when queue is full.
-     */
-    void popAll() noexcept
-    {
-        for (;;) {
-            try {
-                if (pop().empty()) break;
-            } catch (...) {
-            }
-        }
-    }
-};
-
 /**
  * push() caller must be single-thread.
  * pop() caller must be single-thread.
  * Any thread can call quit() and join().
  */
 template<class Conv = PackCompressor, class UnConv = PackUncompressor>
-class ConverterQueueT2
+class ConverterQueueT
 {
     using LockGuard = std::lock_guard<std::mutex>;
     using UniqueLock = std::unique_lock<std::mutex>;
@@ -496,7 +247,7 @@ class ConverterQueueT2
 
 public:
     static constexpr const char* NAME() { return "ConverterQueue"; }
-    ConverterQueueT2(size_t maxQueueNum, size_t threadNum, bool doCompress, int type, size_t para = 0)
+    ConverterQueueT(size_t maxQueueNum, size_t threadNum, bool doCompress, int type, size_t para = 0)
         : maxQueueSize_(maxQueueNum)
         , m_()
         , quit_(false)
@@ -512,7 +263,7 @@ public:
             e.init(doCompress, type, para, &m_, &quit_, &readyQ_, &ready_, &avail_);
         }
     }
-    ~ConverterQueueT2() noexcept {
+    ~ConverterQueueT() noexcept {
         join();
     }
     bool push(compressor::Buffer&& inBuf) {
@@ -574,6 +325,6 @@ public:
 
 } // compressor_local
 
-typedef compressor_local::ConverterQueueT2<> ConverterQueue;
+typedef compressor_local::ConverterQueueT<> ConverterQueue;
 
 } //namespace walb
