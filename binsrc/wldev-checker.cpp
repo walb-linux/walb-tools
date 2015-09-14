acbca0a8734d47cd89ba0760557d203aedc990a8
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2015-09-14 15:59:50 +0900

    wldev-checker: improve retry code.

--- binsrc/wldev-checker.cpp
+++ binsrc/wldev-checker.cpp
@@ -25,6 +25,7 @@ struct Option
     uint64_t bgnLsid;
     size_t pollIntervalMs;
     size_t logIntervalS;
+    size_t retryMs;
     bool dontUseAio;
     uint64_t readStepSize;
     bool isDeleteWlog;
@@ -37,7 +38,8 @@ struct Option
         opt.setDescription("wldev-checker: check wldev.");
         opt.appendParam(&wdevName, "WDEV_NAME", ": walb device name.");
         opt.appendOpt(&bgnLsid, UINT64_MAX, "b", "begin lsid.");
-        opt.appendOpt(&pollIntervalMs, 1000, "i", "poll interval [ms] (defaalt 1000)");
+        opt.appendOpt(&pollIntervalMs, 1000, "i", "poll interval [ms] (default 1000)");
+        opt.appendOpt(&retryMs, 100, "r", "retry interval [ms] (default 100)");
         opt.appendOpt(&logPath, "-", "l", "log output path (default '-')");
         opt.appendBoolOpt(&dontUseAio, "noaio", ": do not use aio");
         opt.appendOpt(&readStepSize, 128 * MEBI, "s", "read size at a step [bytes] (default 128M)");
@@ -81,9 +83,116 @@ void dumpLogPackIo(const std::string& wdevName, uint64_t lsid, size_t i, const L
     tmpFile.save(outPath.str());
 }
 
-void wait(const Option &opt)
+void waitMs(size_t ms)
 {
-    if (opt.pollIntervalMs > 0) util::sleepMs(opt.pollIntervalMs);
+    if (ms > 0) util::sleepMs(ms);
+}
+
+bool isEqualLogPackHeaderImage(const LogPackHeader& packH, const AlignedArray& prevImg)
+{
+    return ::memcmp(packH.rawData(), prevImg.data(), prevImg.size()) == 0;
+}
+
+void retryForeverReadLogPackHeader(
+    const std::string& wdevName, device::SimpleWldevReader& sReader,
+    LogPackHeader& packH, uint64_t lsid, size_t retryMs)
+{
+    const cybozu::Timespec ts0 = cybozu::getNowAsTimespec();
+    LOGs.error() << "invalid logpack header" << wdevName << lsid << ts0;
+    dumpLogPackHeader(wdevName, lsid, packH, ts0);
+    AlignedArray prevImg(packH.pbs());
+    ::memcpy(prevImg.data(), packH.rawData(), prevImg.size());
+
+    size_t c = 0;
+  retry:
+    waitMs(retryMs);
+    c++;
+    sReader.reset(lsid);
+    if (!readLogPackHeader(sReader, packH, lsid)) {
+        if (!isEqualLogPackHeaderImage(packH, prevImg)) {
+            const cybozu::Timespec ts1 = cybozu::getNowAsTimespec();
+            LOGs.info() << "invalid logpack header changed" << wdevName << lsid << ts1 << c;
+            dumpLogPackHeader(wdevName, lsid, packH, ts1);
+            ::memcpy(prevImg.data(), packH.rawData(), prevImg.size());
+        }
+        goto retry;
+    }
+    const cybozu::Timespec ts1 = cybozu::getNowAsTimespec();
+    const cybozu::TimespecDiff td = ts1 - ts0;
+    LOGs.info() << "retry succeeded" << wdevName << lsid << ts0 << ts1 << td << c;
+    dumpLogPackHeader(wdevName, lsid, packH, ts1);
+}
+
+void copyLogPackIo(AlignedArray& dst, const LogBlockShared& src)
+{
+    const size_t pbs = src.pbs();
+    assert(pbs > 0);
+    assert(src.nBlocks() * pbs >= dst.size());
+
+    size_t i = 0;
+    size_t off = 0;
+    while (off < dst.size()) {
+        const size_t s = std::min(dst.size() - off, pbs);
+        ::memcpy(dst.data() + off, src.get(i), s);
+        off += s;
+        i++;
+    }
+}
+
+bool isEqualLogPackIoImage(const LogBlockShared& blockS, const AlignedArray& prevImg)
+{
+    const size_t pbs = blockS.pbs();
+    assert(pbs > 0);
+    assert(blockS.nBlocks() * pbs >= prevImg.size());
+
+    size_t i = 0;
+    size_t off = 0;
+    while (off < prevImg.size()) {
+        const size_t s = std::min(prevImg.size() - off, pbs);
+        if (::memcmp(blockS.get(i), prevImg.data() + off, s) != 0) {
+            return false;
+        }
+        off += s;
+        i++;
+    }
+    return true;
+}
+
+void retryForeverReadLogPackIo(
+    const std::string& wdevName, device::SimpleWldevReader& sReader,
+    const LogPackHeader& packH, size_t i, LogBlockShared& blockS, size_t retryMs)
+{
+    const cybozu::Timespec ts0 = cybozu::getNowAsTimespec();
+    const uint64_t lsid = packH.logpackLsid();
+    LOGs.error() << "invalid logpack IO" << wdevName << lsid << i << ts0;
+    dumpLogPackHeader(wdevName, lsid, packH, ts0);
+    dumpLogPackIo(wdevName, lsid, i, packH, blockS, ts0);
+    const WlogRecord &rec = packH.record(i);
+    AlignedArray prevImg(rec.ioSizeLb() * LBS);
+    copyLogPackIo(prevImg, blockS);
+
+    size_t c = 0;
+retry:
+    waitMs(retryMs);
+    c++;
+    sReader.reset(rec.lsid);
+    blockS.clear();
+    if (!readLogIo(sReader, packH, i, blockS)) {
+        if (!isEqualLogPackIoImage(blockS, prevImg)) {
+            const cybozu::Timespec ts1 = cybozu::getNowAsTimespec();
+            LOGs.info() << "invalid logpack IO changed" << wdevName << lsid << i << ts1 << c;
+            dumpLogPackHeader(wdevName, lsid, packH, ts1);
+            dumpLogPackIo(wdevName, lsid, i, packH, blockS, ts1);
+            copyLogPackIo(prevImg, blockS);
+        }
+        goto retry;
+    }
+    const cybozu::Timespec ts1 = cybozu::getNowAsTimespec();
+    const cybozu::TimespecDiff td = ts1 - ts0;
+    LOGs.info() << "retry succeeded" << wdevName << lsid << i << ts0 << ts1 << td << c;
+    dumpLogPackHeader(wdevName, lsid, packH, ts1);
+    dumpLogPackIo(wdevName, lsid, i, packH, blockS, ts1);
+    blockS.clear();
 }
 
 template <typename Reader>
@@ -125,35 +234,15 @@ void checkWldev(const Option &opt)
         device::LsidSet lsidSet;
         device::getLsidSet(wdevName, lsidSet);
         if (lsid >= lsidSet.permanent) {
-            wait(opt);
+            waitMs(opt.pollIntervalMs);
             continue;
         }
         const uint64_t lsidEnd = std::min(lsid + readStepPb, lsidSet.permanent);
         reader.reset(lsid);
         while (lsid < lsidEnd) {
             if (!readLogPackHeader(reader, packH, lsid)) {
-                const cybozu::Timespec ts0 = cybozu::getNowAsTimespec();
-#if 0
-                LOGs.error() << "invalid logpack header" << wdevName << lsid << ts0;
-                dumpLogPackHeader(wdevName, lsid, packH, ts0);
-#endif
-
-                int c = 0;
-              retry2:
-                c++;
-                sReader.reset(lsid);
-                if (!readLogPackHeader(sReader, packH, lsid)) {
-                    goto retry2;
-                }
-                const cybozu::Timespec ts1 = cybozu::getNowAsTimespec();
-                const cybozu::TimespecDiff td = ts1 - ts0;
-                LOGs.info() << "retry succeeded" << wdevName << lsid << ts0 << ts1 << td << c;
-                reader.reset(lsid + 1);
-#if 0
-                wait(opt);
-                reader.reset(lsid);
-                continue;
-#endif
+                retryForeverReadLogPackHeader(wdevName, sReader, packH, lsid, opt.retryMs);
+                reader.reset(lsid + 1); // for next read.
             }
             if (opt.skipLogIos) {
                 skipAllLogIos(reader, packH);
@@ -162,16 +251,9 @@ void checkWldev(const Option &opt)
                     const WlogRecord &rec = packH.record(i);
                     if (!rec.hasData()) continue;
                     LogBlockShared blockS;
-                  retry:
                     if (!readLogIo(reader, packH, i, blockS)) {
-                        const cybozu::Timespec ts = cybozu::getNowAsTimespec();
-                        LOGs.error() << "invalid logpack IO" << wdevName << lsid << i << ts;
-                        dumpLogPackHeader(wdevName, lsid, packH, ts);
-                        dumpLogPackIo(wdevName, lsid, i, packH, blockS, ts);
-                        wait(opt);
-                        reader.reset(packH.record(i).lsid);
-                        blockS.clear();
-                        goto retry;
+                        retryForeverReadLogPackIo(wdevName, sReader, packH, i, blockS, opt.retryMs);
+                        reader.reset(rec.lsid + rec.ioSizePb(packH.pbs())); // for next read.
                     }
                 }
             }
