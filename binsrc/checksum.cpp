1f1c19ecc4c6614af80476ca795898ed12032ff7
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2015-10-16 16:26:03 +0900

    checksum: add -bulk option.

--- binsrc/checksum.cpp
+++ binsrc/checksum.cpp
@@ -13,6 +13,7 @@ struct Option
     std::string saltStr;
     std::string ioDataPath;
     uint32_t salt;
+    size_t bulkSize;
     bool isHex;
     bool isDebug;
 
@@ -20,6 +21,7 @@ struct Option
         cybozu::Option opt;
         opt.setDescription("Calculate checksum of IO data.\n");
         opt.appendOpt(&saltStr, "", "salt", ": checksum salt. prefix 0x means hex.");
+        opt.appendOpt(&bulkSize, 0, "bulk", ": put checksum for each bulk with size [bytes]. (default: whole file)");
         opt.appendBoolOpt(&isHex, "hex", ": put checksum in hex.");
         opt.appendBoolOpt(&isDebug, "debug", ": put debug messages to stderr.");
         opt.appendParamOpt(&ioDataPath, "-", "IO_DATA_FILE", ": IO data dump file.");
@@ -36,6 +38,17 @@ struct Option
     }
 };
 
+void calcCsumAndPut(const char *data, size_t size, const Option& opt, uint64_t bulkId = size_t(-1))
+{
+    const uint32_t csum = cybozu::util::calcChecksum(data, size, opt.salt);
+    if (bulkId != uint64_t(-1)) ::printf("%" PRIu64 "\t", bulkId);
+    if (opt.isHex) {
+        ::printf("%08x\n", csum);
+    } else {
+        ::printf("%u\n", csum);
+    }
+}
+
 int doMain(int argc, char* argv[])
 {
     Option opt(argc, argv);
@@ -48,14 +61,27 @@ int doMain(int argc, char* argv[])
         file.open(opt.ioDataPath, O_RDONLY);
     }
 
-    std::vector<char> buf;
-    readAllFromFile(file, buf);
-    const uint32_t csum = cybozu::util::calcChecksum(buf.data(), buf.size(), opt.salt);
-
-    if (opt.isHex) {
-        ::printf("%08x\n", csum);
+    if (opt.bulkSize == 0) {
+        std::vector<char> buf;
+        cybozu::util::readAllFromFile(file, buf);
+        calcCsumAndPut(buf.data(), buf.size(), opt);
     } else {
-        ::printf("%u\n", csum);
+        std::vector<char> buf(opt.bulkSize);
+        size_t off = 0;
+        size_t bulkId = 0;
+        for (;;) {
+            const size_t r = file.readsome(&buf[off], buf.size() - off);
+            if (r == 0) {
+                if (off > 0) calcCsumAndPut(buf.data(), off, opt, bulkId);
+                break;
+            }
+            off += r;
+            if (off == buf.size()) {
+                calcCsumAndPut(buf.data(), buf.size(), opt, bulkId);
+                off = 0;
+                bulkId++;
+            }
+        }
     }
     return 0;
 }
