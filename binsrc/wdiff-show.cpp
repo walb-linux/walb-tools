9e5af9a6d8991eba25d75ea1b9194c684809f996
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2017-03-14 18:10:19 +0900

    indexed-wdiff: binsrc/wdiff-show.cpp

--- binsrc/wdiff-show.cpp
+++ binsrc/wdiff-show.cpp
@@ -10,12 +10,13 @@
 #include "walb_diff_stat.hpp"
 #include "cybozu/option.hpp"
 #include "walb_util.hpp"
+#include "fileio.hpp"
 
 using namespace walb;
 
 struct Option
 {
-    bool isDebug, doSearch, doStat, noHead, noRec;
+    bool isDebug, doSearch, doStat, noHead, noRec, isIndexed;
     uint64_t addr;
     std::string filePath;
     std::vector<std::string> filePathV;
@@ -34,6 +35,7 @@ struct Option
         opt.appendBoolOpt(&noHead, "nohead", ": does not put header..");
         opt.appendBoolOpt(&noRec, "norec", "; does not put records.");
         opt.appendBoolOpt(&isDebug, "debug", ": put debug messages.");
+        opt.appendBoolOpt(&isIndexed, "indexed", ": use indexed format instead of sorted format.");
         opt.appendParamVec(&filePathV, "WDIFF_PATH_LIST", ": wdiff file list (default: stdin)");
         opt.appendHelp("h", ": put this message.");
 
@@ -44,7 +46,8 @@ struct Option
     }
 };
 
-inline bool matchAddress(uint64_t addr, const DiffRecord& rec)
+template <typename Record>
+inline bool matchAddress(uint64_t addr, const Record& rec)
 {
     return rec.io_address <= addr && addr < rec.endIoAddress();
 }
@@ -68,10 +71,49 @@ void printWdiff(DiffReader &reader, DiffStatistics &stat, const Option &opt)
     }
 }
 
+
+void printIndexedWdiff(IndexedDiffReader &reader, const Option &opt)
+{
+    DiffFileHeader wdiffH;
+    wdiffH = reader.header();
+    if (!opt.noHead) wdiffH.print();
+
+    DiffIndexRecord rec;
+    AlignedArray data;
+    while (reader.readDiff(rec, data)) {
+        if (!opt.doSearch || matchAddress(opt.addr, rec)) {
+            if (!opt.noRec) {
+                if (!rec.isValid()) ::printf("Invalid record: ");
+                rec.printOneline();
+            }
+            //if (opt.doStat) stat.update(rec); // QQQ
+        }
+    }
+}
+
+
+void printIndexedWdiffs(const Option &opt)
+{
+    IndexedDiffReader reader;
+
+    if (opt.filePathV.empty()) {
+        throw cybozu::Exception("Indexed format does not support stream.");
+    }
+    for (const std::string &path : opt.filePathV) {
+        reader.setFile(cybozu::util::File(path, O_RDWR));
+        printIndexedWdiff(reader, opt);
+    }
+}
+
+
 int doMain(int argc, char *argv[])
 {
     Option opt(argc, argv);
     util::setLogSetting("-", opt.isDebug);
+    if (opt.isIndexed) {
+        printIndexedWdiffs(opt);
+        return 0;
+    }
 
     DiffReader reader;
     DiffStatistics stat;
