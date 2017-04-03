8a361f51e8288d4c0bc011ae16e3cee1026a3190
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2017-04-03 15:31:27 +0900

    wdiff-full: support -indexed option.

--- binsrc/wdiff-full.cpp
+++ binsrc/wdiff-full.cpp
@@ -11,6 +11,34 @@
 
 using namespace walb;
 
+struct Option
+{
+    std::string input, output;
+    bool isIndexed;
+    size_t ioSize;
+
+    Option(int argc, char *argv[]) {
+        cybozu::Option opt;
+        opt.setUsage("Usage: wdiff-full -i [full image] -o [wdiff]", true);
+        opt.appendOpt(&input, "-", "i", ": input full image (default: stdin)");
+        opt.appendOpt(&output, "-", "o", ": output diff file (default: stdout)");
+        opt.appendOpt(&ioSize, 64 * KIBI, "ios", ": IO size in bytes (default 64K)");
+        opt.appendBoolOpt(&isIndexed, "indexed", ": use indexed format instead of sorted format.");
+        opt.appendHelp("h");
+
+        if (!opt.parse(argc, argv)) {
+            opt.usage();
+            ::exit(1);
+        }
+        if (ioSize == 0 || ioSize % LOGICAL_BLOCK_SIZE != 0) {
+            ::fprintf(::stderr, "ioSize must be multiples of logical block size %u.\n"
+                      , LOGICAL_BLOCK_SIZE);
+            ::exit(1);
+        }
+    }
+};
+
+
 class FullImageToWalbDiffConverter
 {
 private:
@@ -24,30 +52,42 @@ public:
         , ioSize_(ioBlocks * LOGICAL_BLOCK_SIZE)
         , buf0_(ioSize_) {
     }
-    void convert(int inFd, int outFd) {
-        cybozu::util::File reader(inFd);
-        DiffWriter writer(outFd);
+    void run(cybozu::util::File &inFile, cybozu::util::File &outFile, bool isIndexed) {
+        DiffWriter sWriter;
+        IndexedDiffWriter iWriter;
+        if (isIndexed) {
+            iWriter.setFd(outFile.fd());
+        } else {
+            sWriter.setFd(outFile.fd());
+        }
+
         DiffFileHeader head;
         head.setMaxIoBlocksIfNecessary(ioBlocks_);
-        writer.writeHeader(head);
+        if (isIndexed) {
+            iWriter.writeHeader(head);
+        } else {
+            sWriter.writeHeader(head);
+        }
 
         uint64_t ioAddr = 0;
-        uint32_t blks = readChunk(reader);
+        uint32_t blks = readChunk(inFile);
         while (0 < blks) {
-            DiffRecord rec;
-            rec.io_address = ioAddr;
-            rec.io_blocks = blks;
-            rec.setNormal();
-            rec.data_size = blks * LOGICAL_BLOCK_SIZE;
-            rec.checksum = 0; // calculated later.
-
-            writer.compressAndWriteDiff(rec, buf0_.data());
-
+            if (isIndexed) {
+                writeIndexedDiff(iWriter, ioAddr, blks);
+            } else {
+                writeSortedDiff(sWriter, ioAddr, blks);
+            }
             ioAddr += blks;
-            blks = readChunk(reader);
+            blks = readChunk(inFile);
         }
-        writer.close();
+
+        if (isIndexed) {
+            iWriter.finalize();
+        } else {
+            sWriter.close();
+        }
     }
+
 private:
     /**
      * RETURN:
@@ -68,16 +108,56 @@ private:
         }
         return c;
     }
+    void writeSortedDiff(DiffWriter& writer, uint64_t addr, uint32_t blks) {
+        DiffRecord rec;
+        rec.io_address = addr;
+        rec.io_blocks = blks;
+        rec.setNormal();
+        rec.compression_type = ::WALB_DIFF_CMPR_NONE;
+        rec.data_size = blks * LOGICAL_BLOCK_SIZE;
+        rec.checksum = 0; // calculated later.
+        writer.compressAndWriteDiff(rec, buf0_.data());
+    }
+    void writeIndexedDiff(IndexedDiffWriter& writer, uint64_t addr, uint32_t blks) {
+        IndexedDiffRecord rec;
+        rec.io_address = addr;
+        rec.io_blocks = blks;
+        rec.setNormal();
+        rec.compression_type = ::WALB_DIFF_CMPR_NONE;
+        rec.data_size = blks * LOGICAL_BLOCK_SIZE;
+        rec.orig_blocks = blks;
+        rec.io_offset = 0;
+        rec.io_checksum = 0; // calculated later.
+        rec.rec_checksum = 0; // calculated later.
+        writer.compressAndWriteDiff(rec, buf0_.data());
+    }
 };
 
-int doMain(int argc, char *[])
+void setupFiles(const std::string& inName, const std::string& outName,
+                cybozu::util::File& inFile, cybozu::util::File& outFile)
 {
-    if (1 < argc) {
-        ::printf("Usage: wdiff-full < [full image] > [wdiff]\n");
-        return 1;
+    if (inName == "-") {
+        inFile.setFd(0);
+    } else {
+        inFile.open(inName, O_RDONLY);
     }
-    FullImageToWalbDiffConverter c(64 * 1024 / LOGICAL_BLOCK_SIZE);
-    c.convert(0, 1);
+    if (outName == "-") {
+        outFile.setFd(1);
+    } else {
+        outFile.open(outName, O_CREAT | O_TRUNC | O_RDWR, 0644);
+    }
+}
+
+int doMain(int argc, char *argv[])
+{
+    Option opt(argc, argv);
+
+    cybozu::util::File inFile, outFile;
+    setupFiles(opt.input, opt.output, inFile, outFile);
+
+    FullImageToWalbDiffConverter c(opt.ioSize / LOGICAL_BLOCK_SIZE);
+    c.run(inFile, outFile, opt.isIndexed);
+
     return 0;
 }
 
