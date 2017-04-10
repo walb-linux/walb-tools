945ef35294a4084413adf27ff1997e29697adecb
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2017-04-10 16:25:39 +0900

    wdiff-merge: support stdout output.

--- binsrc/wdiff-merge.cpp
+++ binsrc/wdiff-merge.cpp
@@ -24,7 +24,7 @@ struct Option : public cybozu::Option
         setDescription("Merge wdiff files.");
         appendOpt(&maxIoSize, 0, "x", "SIZE: max IO size [byte]. 0 means no limitation.");
         appendVec(&inputWdiffs, "i", "WDIFF_PATH_LIST: input wdiff paths.");
-        appendMust(&outputWdiff, "o", "WDIFF_PATH: output wdiff path.");
+        appendOpt(&outputWdiff, "-", "o", "WDIFF_PATH: output wdiff path (default: stdout).");
         appendBoolOpt(&doStat, "stat", ": put statistics.");
         appendOpt(&cmprStr, "snappy:0:1", "cmpr", "type:level:concurrency : compression for output (default: snappy:0:1)");
         appendHelp("h", ": put this message.");
@@ -40,7 +40,7 @@ struct Option : public cybozu::Option
             goto error;
         }
         if (inputWdiffs.empty()) {
-            ::printf("You must specify one or more input wdiff files.\n");
+            ::fprintf(::stderr, "You must specify one or more input wdiff files.\n");
             goto error;
         }
         cmpr.parse(cmprStr);
@@ -59,13 +59,25 @@ int doMain(int argc, char *argv[])
     for (std::string &path : opt.inputWdiffs) {
         merger.addWdiff(path);
     }
-    cybozu::util::File file(opt.outputWdiff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    cybozu::util::File file;
+    if (opt.outputWdiff == "-") {
+        file.setFd(1);
+    } else {
+        file.open(opt.outputWdiff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    }
     merger.setMaxIoBlocks(opt.maxIoBlocks());
     merger.setShouldValidateUuid(false);
 #if 0
     merger.mergeToFd(file.fd());
 #else
     merger.mergeToFdInParallel(file.fd(), opt.cmpr);
+#endif
+#if 0
+    /*
+     * currently we prefer prompt quit of the command
+     * rather than persistence of output file.
+     */
+    file.fdatasync();
 #endif
     file.close();
     if (opt.doStat) {
