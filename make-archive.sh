6e75fbeb9103d68d530b3aeea6e2cb7e9b39413e
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2017-05-09 16:38:19 +0900

    update make-archive.sh

--- make-archive.sh
+++ make-archive.sh
@@ -7,6 +7,6 @@ tarfile=${name}_${version}.tar
 
 git archive HEAD --format=tar --prefix $tarprefix -o $tarfile
 tar f $tarfile --wildcards --delete '*/.gitignore'
-tar rf $tarfile -h cybozulib/include/cybozu --transform "s|^|${tarprefix}|g"
-tar rf $tarfile -h walb/include --transform "s|^|${tarprefix}|g"
+#tar rf $tarfile -h cybozulib/include/cybozu --transform "s|^|${tarprefix}|g"
+#tar rf $tarfile -h walb/include --transform "s|^|${tarprefix}|g"
 gzip $tarfile
