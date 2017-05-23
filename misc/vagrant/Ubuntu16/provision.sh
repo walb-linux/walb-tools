0ff3ae9a61484c9a90f83d900f5c109c20ff79fa
MITSUNARI Shigeo <herumi@nifty.com>
2017-05-23 15:48:16 +0900

    change download url

--- misc/vagrant/Ubuntu16/provision.sh
+++ misc/vagrant/Ubuntu16/provision.sh
@@ -4,7 +4,8 @@ sudp apt update -y
 sudo apt install -y linux-headers-`uname -r` libaio-dev libsnappy-dev liblzma-dev zlib1g-dev python make gcc g++ ipython
 
 # download setup script
-BASE_URL=https://github.dev.cybozu.co.jp/raw/herumi/walb-tools/master/
+#BASE_URL=https://github.dev.cybozu.co.jp/raw/herumi/walb-tools/master/
+BASE_URL=https://github.com/raw/walb-linux/walb-tools/master/
 wget ${BASE_URL}misc/vagrant/setup.sh
 chmod +x setup.sh
 sudo reboot
