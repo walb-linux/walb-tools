ea7178d929522a1621aaa96b682ea2b1ae20bc0b
MITSUNARI Shigeo <herumi@nifty.com>
2017-05-25 15:53:35 +0900

    update url

--- misc/vagrant/CentOS7/provision.sh
+++ misc/vagrant/CentOS7/provision.sh
@@ -9,7 +9,7 @@ sudo pip install pylint
 
 # download setup script
 #BASE_URL=https://github.dev.cybozu.co.jp/raw/herumi/walb-tools/master/
-BASE_URL=https://github.com/raw/walb-linux/walb-tools/master/
+BASE_URL=https://raw.githubusercontent.com/walb-linux/walb-tools/master/
 wget ${BASE_URL}misc/vagrant/setup.sh
 chmod +x setup.sh
 sudo reboot
--- misc/vagrant/Ubuntu16/provision.sh
+++ misc/vagrant/Ubuntu16/provision.sh
@@ -5,7 +5,7 @@ sudo apt install -y linux-headers-`uname -r` libaio-dev libsnappy-dev liblzma-de
 
 # download setup script
 #BASE_URL=https://github.dev.cybozu.co.jp/raw/herumi/walb-tools/master/
-BASE_URL=https://github.com/raw/walb-linux/walb-tools/master/
+BASE_URL=https://raw.githubusercontent.com/walb-linux/walb-tools/master/
 wget ${BASE_URL}misc/vagrant/setup.sh
 chmod +x setup.sh
 sudo reboot
