93e3dcac425599c7d122315029e5156fa2f7bd9f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2018-03-19 14:04:36 +0900

    fix: use cybozu::readSome() and cybozu::write() to work well with the latest cybouzlib.

--- src/fileio_serializer.hpp
+++ src/fileio_serializer.hpp
@@ -6,35 +6,24 @@
  *
  * (C) 2013 Cybozu Labs, Inc.
  */
-#include "cybozu/stream_fwd.hpp"
+#include "cybozu/stream.hpp"
 #include "fileio.hpp"
 
 namespace cybozu {
 
-#define WALB_FILEIO_DEFINE_SERIALIZE_LOADER(type)                       \
-    template <>                                                         \
-    struct InputStreamTag<type>                                         \
-    {                                                                   \
-        static inline size_t readSome(type &is, void *buf, size_t size) { \
-            return is.readsome(buf, size);                              \
-        }                                                               \
-    }
 
-WALB_FILEIO_DEFINE_SERIALIZE_LOADER(util::File);
+template <>
+inline size_t readSome<util::File>(void *buf, size_t size, util::File& is)
+{
+    return is.readsome(buf, size);
+}
 
-#undef WALB_FILEIO_DEFINE_SERIALIZE_LOADER
 
-#define WALB_FILEIO_DEFINE_SERIALIZE_SAVER(type)                        \
-    template <>                                                         \
-    struct OutputStreamTag<type>                                        \
-    {                                                                   \
-        static inline void write(type &os, const void *buf, size_t size) { \
-            os.write(buf, size);                                        \
-        }                                                               \
-    }
+template <>
+inline void write<util::File>(util::File& os, const void *buf, size_t size)
+{
+    os.write(buf, size);
+}
 
-WALB_FILEIO_DEFINE_SERIALIZE_SAVER(util::File);
-
-#undef WALB_FILEIO_DEFINE_SERIALIZE_SAVER
 
 } //namespace cybozu
--- src/tmp_file_serializer.hpp
+++ src/tmp_file_serializer.hpp
@@ -6,28 +6,27 @@
  *
  * (C) 2013 Cybozu Labs, Inc.
  */
-#include "cybozu/stream_fwd.hpp"
+#include "cybozu/stream.hpp"
 #include "tmp_file.hpp"
 #include "fileio.hpp"
 
 namespace cybozu {
 
-template <>
-struct InputStreamTag<TmpFile>
-{
-    static inline size_t readSome(TmpFile &is, void *buf, size_t size) {
-        util::File file(is.fd());
-        return file.readsome(buf, size);
-    }
-};
 
 template <>
-struct OutputStreamTag<TmpFile>
+inline size_t readSome<TmpFile>(void *buf, size_t size, TmpFile& is)
 {
-    static inline void write(TmpFile &os, const void *buf, size_t size) {
-        util::File file(os.fd());
-        file.write(buf, size);
-    }
-};
+    util::File file(is.fd());
+    return file.readsome(buf, size);
+}
+
+
+template <>
+inline void write<TmpFile>(TmpFile& os, const void *buf, size_t size)
+{
+    util::File file(os.fd());
+    file.write(buf, size);
+}
+
 
 } //namespace cybozu
